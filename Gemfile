e1e05946fd936ad6884861e1d762b6dde54ed5c8
Eric Ryan <ewryan@gmail.com>
2016-04-10 09:10:46 -0600

    initial commit

---
+++ Gemfile
@@ -0,0 +1,6 @@
+source "http://rubygems.org"
+
+gem "mechanize"
+gem "awesome_print"
+gem "ruby-trello"
+gem "launchy"
---
+++ main.rb
@@ -0,0 +1,67 @@
+require 'bundler'
+require 'mechanize'
+require 'awesome_print'
+require 'trello'
+require 'yaml'
+
+config = YAML::load_file(File.join(__dir__, 'config.yml'))
+
+trello_api_key = config["trello"]["api_key"]
+trello_token = config["trello"]["token"]
+
+include Trello
+
+Trello.configure do |config|
+  config.developer_public_key = trello_api_key 
+  config.member_token = trello_token 
+end
+
+eric = Member.find(config["trello"]["member_name"])
+
+board = eric.boards.select {|b| b.name==config["trello"]["board"]}.first
+list = board.lists.select {|l| l.name==config["trello"]["list"]}.first
+
+
+m = Mechanize.new
+m.user_agent_alias = "Mac Firefox"
+page = m.get(config["web"]["url"])
+
+form = page.forms.first
+
+form['loginName'] = config["web"]["username"]
+form['password'] = config["web"]["password"]
+
+page = form.submit
+
+raw_results = page.search('div.view-details-link-ct a')
+
+raw_results.each do |result|
+	href = result.attr('href')
+	details_page = m.get "http://www.iresis.com#{href}"
+    # ap details_page 
+
+	street = details_page.search('span.street').text
+	city = details_page.search('span.city').text
+	state = details_page.search('span.state').text
+	zip = details_page.search('span.zip').text
+	price = details_page.search('div.overview h3').text
+	img = details_page.at_css('img').attr('src')
+	url = details_page.uri
+	description = details_page.at_css('div.description').text + "\r\n#{url}"
+
+	puts "#{price} - #{street} #{city} #{state} #{zip}"
+	puts description
+
+	card = Card.create(
+		name: "#{price} - #{street} #{city}, #{state} #{zip}", 
+		list_id: list.id,
+		desc: description 
+	)
+	card.add_attachment(img)
+
+	break
+end
+
+#TODO
+# deduping
+# production
