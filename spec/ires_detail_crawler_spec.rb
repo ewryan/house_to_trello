f9dafa8d36ea20a57a37404db1650ae8c5fa88f8
Eric Ryan <ewryan@gmail.com>
2016-04-10 20:56:30 -0600

    er: more tests

--- spec/ires_detail_crawler_spec.rb
+++ spec/ires_detail_crawler_spec.rb
@@ -6,11 +6,23 @@ require "lib/models/ires_detail_crawler"
 describe IRESDetailCrawler do
   let(:client) {IRESDetailCrawler.new config, Mechanize.new, "http://www.iresis.com/cwa/mysite/inbox/details?lgid=23634503&lid=1086715"}
 
-  describe "#run" do
-    it "extracts address" do
-      VCR.use_cassette('details/lgid=23634503&lid=1086715') do
-        result = client.run
-        expect(result[:street]).to eq("802 W Dahlia Ct")
+  def node_matches key, value, cassette
+    VCR.use_cassette(cassette) do
+      result = client.run
+      expect(result[key]).to eq(value)
+    end
+  end
+
+  cassettes = ['details/lgid=23634503&lid=1086715']
+
+  cassettes.each do |cassette|
+    describe "#run" do
+      it "extracts street" do
+        node_matches :street, "802 W Dahlia Ct", cassette
+      end
+
+      it "extracts city" do
+        node_matches :city, "Louisville", cassette
       end
     end
   end
